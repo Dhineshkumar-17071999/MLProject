91016fc8114eb0d49d7bf8b8cd966636e49deaa0
Dhineshkumar P <dhineshkumar.mailme@gmail.com>
2023-05-15 20:17:18 +0530

    HyperParamet Training Done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -19,12 +20,16 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, X_test, y_train, y_test, models):
+def evaluate_model(X_train, X_test, y_train, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            Grid_Search = GridSearchCV(model, para, cv = 3)
+            Grid_Search.fit(X_train, y_train)
+            model.set_params(**Grid_Search.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
